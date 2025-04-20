85da64825243958c9fa90404b33a44cc420fa8f7
YH <imrubi5555@gmail.com>
2025-04-20 23:15:46 +0900

    Style: RootLayout 패딩값 20px

--- src/shared/layouts/root-layout.tsx
+++ src/shared/layouts/root-layout.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import styled from "@emotion/styled";
 
 export default function RootLayout() {
   return (
-    <div>
+    <main>
       <Header />
-      <Outlet />
-    </div>
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
   );
 }
+
+const Container = styled.div`
+  padding: 20px;
+`;
