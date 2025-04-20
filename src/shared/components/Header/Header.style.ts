61b00a28d850cd66d99635bbcd32dffd16cb1c9d
YH <imrubi5555@gmail.com>
2025-04-20 22:47:25 +0900

    Feat: 헤더바 구현

---
+++ src/shared/components/Header/Header.style.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.nav`
+  padding: 20px;
+  padding-top: 8px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #d7d7d7;
+`;
+
+export const Title = styled.h1`
+  margin: 0 auto;
+  color: #121212;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.5px;
+`;
--- src/shared/layouts/root-layout.tsx
+++ src/shared/layouts/root-layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import Header from "../components/Header";
 
 export default function RootLayout() {
   return (
     <div>
-      root layout
+      <Header />
       <Outlet />
     </div>
   );
