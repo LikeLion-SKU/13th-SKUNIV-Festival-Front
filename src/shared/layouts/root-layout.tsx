b506ef1cc7e058b91f2bcabfa636d3a211180871
Taegyeong Park <tgyeongt@gmail.com>
2025-04-26 22:38:09 +0900

    ✨ Feat: 메인페이지 퍼블리싱 완료 (인터렉션 수정 필요)

--- src/shared/layouts/root-layout.tsx
+++ src/shared/layouts/root-layout.tsx
@@ -4,18 +4,12 @@ import styled from "@emotion/styled";
 
 export default function RootLayout() {
   return (
-<<<<<<< HEAD
-    <div>
-      <Outlet />
-    </div>
-=======
     <main>
       <Header />
       <Container>
         <Outlet />
       </Container>
     </main>
->>>>>>> 767724c3fead2ce2216c00d352b813e22550dea8
   );
 }
 
