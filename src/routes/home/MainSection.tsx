08baf0779a6dd888b2d1e29c95542f769432d7f9
Taegyeong Park <tgyeongt@gmail.com>
2025-04-26 18:51:44 +0900

    feature: 초기 언어 설정 페이지 퍼블리싱

--- src/routes/home/MainSection.tsx
+++ src/routes/home/MainSection.tsx
@@ -4,52 +4,89 @@ import styled from "styled-components";
 import mainImg from "@icon/main_title.svg";
 import flower1 from "@icon/flower_1.svg";
 import flower2 from "@icon/flower_2.svg";
+import ko from "@icon/ko.svg";
+import en from "@icon/en.svg";
+import ch from "@icon/ch.svg";
+import jp from "@icon/jp.svg";
 
-export default function MainSection() {
+interface MainSectionProps {
+  onSelectLang: (lang: string) => void;
+  langSelected: boolean;
+}
+
+export default function MainSection({ onSelectLang, langSelected }: MainSectionProps) {
   const flower1Ref = useRef<HTMLImageElement>(null);
   const flower2Ref = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    gsap.fromTo(
-      flower1Ref.current,
-      { scale: 0, rotation: 0, opacity: 0 },
-      {
-        scale: 1,
-        rotation: 360,
-        opacity: 1,
-        duration: 1,
-        ease: "back.out(1.7)",
-      }
-    );
+    if (langSelected) {
+      gsap.fromTo(
+        flower1Ref.current,
+        { scale: 0, rotation: 0, opacity: 0 },
+        {
+          scale: 1,
+          rotation: 360,
+          opacity: 1,
+          duration: 1,
+          ease: "back.out(1.7)",
+        }
+      );
 
-    gsap.fromTo(
-      flower2Ref.current,
-      { scale: 0, rotation: 0, opacity: 0 },
-      {
-        scale: 1,
-        rotation: -360,
-        opacity: 1,
-        duration: 1,
-        delay: 0.4,
-        ease: "back.out(1.7)",
-      }
-    );
-  }, []);
+      gsap.fromTo(
+        flower2Ref.current,
+        { scale: 0, rotation: 0, opacity: 0 },
+        {
+          scale: 1,
+          rotation: -360,
+          opacity: 1,
+          duration: 1,
+          delay: 0.4,
+          ease: "back.out(1.7)",
+        }
+      );
+    }
+  }, [langSelected]);
 
   return (
     <MainWrapper>
       <Flower src={flower1} alt="flower1" ref={flower1Ref} className="flower1" />
       <Flower src={flower2} alt="flower2" ref={flower2Ref} className="flower2" />
-      <img className="img" src={mainImg} alt={mainImg} />
-      <p className="title">2025 서경대학교 대동제</p>
-      <p className="date">05.07~05.09</p>
+      <img className="img" src={mainImg} alt="main" />
+      {langSelected ? (
+        <IntroWrapper>
+          <p className="title">2025 서경대학교 대동제</p>
+          <p className="date">05.07~05.09</p>
+        </IntroWrapper>
+      ) : (
+        <LanguageWrapper>
+          <p className="text_32">welcome!</p>
+          <p className="text_20">Please select language.</p>
+          <p className="text_16">언어를 선택해주세요.</p>
+
+          <LangButton onClick={() => onSelectLang("ko")}>
+            <img className="img" src={ko} alt="ko" />
+            한국어
+          </LangButton>
+          <LangButton onClick={() => onSelectLang("en")}>
+            <img className="img" src={en} alt="en" />
+            English
+          </LangButton>
+          <LangButton onClick={() => onSelectLang("ch")}>
+            <img className="img" src={ch} alt="ch" />
+            中文
+          </LangButton>
+          <LangButton onClick={() => onSelectLang("jp")}>
+            <img className="img" src={jp} alt="jp" />
+            にほんご
+          </LangButton>
+        </LanguageWrapper>
+      )}
     </MainWrapper>
   );
 }
 
 const MainWrapper = styled.div`
   position: relative;
-  font-family: "HSSanTokki20-Regular", sans-serif;
   text-align: center;
   color: white;
   padding-top: 90px;
@@ -59,14 +96,6 @@ const MainWrapper = styled.div`
   & > .img {
     position: relative;
     z-index: 2;
-  }
-
-  & > .title {
-    font-size: 14px;
-  }
-
-  & > .date {
-    font-size: 12px;
   }
 `;
 
@@ -83,5 +112,64 @@ const Flower = styled.img`
   &.flower2 {
     top: 123px;
     right: 25px;
+  }
+`;
+
+const IntroWrapper = styled.div`
+  font-family: "HSSanTokki20-Regular", sans-serif;
+  & > .title {
+    font-size: 14px;
+  }
+
+  & > .date {
+    font-size: 12px;
+  }
+`;
+
+const LanguageWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > .text_32 {
+    font-size: 32px;
+    font-weight: 800;
+    line-height: 197%;
+  }
+
+  & > .text_20 {
+    font-size: 20px;
+    font-weight: 600;
+    letter-spacing: -0.2px;
+    margin-bottom: 4px;
+  }
+  & > .text_16 {
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: -0.2px;
+    margin-bottom: 20px;
+  }
+`;
+
+const LangButton = styled.div`
+  width: 161px;
+  height: 50px;
+  background-color: rgba(255, 255, 255, 0.88);
+  padding: 8px 12px;
+  margin-top: 16px;
+  border-radius: 46px;
+  cursor: pointer;
+  color: #111;
+  font-size: 16px;
+  font-weight: 600;
+  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  & > .img {
+    margin-right: 30px;
   }
 `;
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import MainSection from "./MainSection";
 import Section from "./Section";
@@ -6,19 +7,29 @@ import LineUp from "./LineUp";
 import Booth from "./Booth";
 
 export default function Home() {
+  const [lang, setLang] = useState<string | null>(null);
+
+  const handleLangSelect = (selectedLang: string) => {
+    setLang(selectedLang);
+  };
   return (
-    <HomeWrapper>
-      <MainSection />
-      <Section title="축제 소개" content="이번 축제는 어쩌구 저쩌구 어쩌구 저쩌구" />
-      <Section title="공연 시간표" content={<TimeTable />} />
-      <Section title="라인업" content={<LineUp />} />
-      <Section title="부스 보러가기" content={<Booth />} />
-      <Section title="about" content="?" />
+    <HomeWrapper langSelected={!!lang}>
+      <MainSection onSelectLang={handleLangSelect} langSelected={!!lang} />
+      {lang && (
+        <>
+          <Section title="축제 소개" content="이번 축제는 어쩌구 저쩌구 어쩌구 저쩌구" />
+          <Section title="공연 시간표" content={<TimeTable />} />
+          <Section title="라인업" content={<LineUp />} />
+          <Section title="부스 보러가기" content={<Booth />} />
+          <Section title="about" content="?" />
+        </>
+      )}
     </HomeWrapper>
   );
 }
 
-const HomeWrapper = styled.div`
-  height: 100%;
-  background: linear-gradient(180deg, #1e3dc7 35.58%, #97f5ff 100%);
+const HomeWrapper = styled.div<{ langSelected: boolean }>`
+  height: ${({ langSelected }) => (langSelected ? "auto" : "100vh")};
+  background: ${({ langSelected }) =>
+    langSelected ? "linear-gradient(180deg, #1e3dc7 35.58%, #97f5ff 100%)" : "#1E3DC7"};
 `;
