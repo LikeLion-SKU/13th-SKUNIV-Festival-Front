8be75817edbcf9b52df2eb5365439932860fd93d
YH <imrubi5555@gmail.com>
2025-05-06 04:24:52 +0900

    Feat: 부스 예약 페이지 시간대마다 예약 정지 & 번역

--- src/routes/boothdetail/ReservationButton.tsx
+++ src/routes/boothdetail/ReservationButton.tsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import useReservationStore from "../../shared/stores/useReservationStore";
+import { useTranslation } from "react-i18next";
+import { useBreakTime } from "../../shared/hooks/useBreakTime";
 
 interface ReservationButtonProps {
   disabled?: boolean;
@@ -8,10 +10,25 @@ interface ReservationButtonProps {
 const ReservationButton = ({ disabled }: ReservationButtonProps) => {
   const { setModalStep } = useReservationStore();
 
+  const { t } = useTranslation("booth");
+
+  const isDayBreakTime = useBreakTime("16:30", "16:59");
+
+  const isNotAvailableTime = useBreakTime("23:00", "9:59");
+
   return (
     <Footer>
-      <Button disabled={disabled} onClick={() => !disabled && setModalStep(1)}>
-        {disabled ? "해당 부스는 예약 서비스를 운영하지 않습니다." : "예약하기"}
+      <Button
+        disabled={disabled || isDayBreakTime || isNotAvailableTime}
+        onClick={() => !disabled && setModalStep(1)}
+      >
+        {disabled
+          ? t("no_reservation")
+          : isDayBreakTime
+          ? t("day_break_time")
+          : isNotAvailableTime
+          ? t("reservation_unavailable")
+          : t("reserve")}
       </Button>
     </Footer>
   );
--- src/routes/tabling/Card.tsx
+++ src/routes/tabling/Card.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { BoothResponse } from ".";
 import Br from "../../shared/components/Br";
 import * as S from "./Card.style";
@@ -7,10 +8,12 @@ interface CardProps {
 }
 
 const Card = ({ data }: CardProps) => {
+  const { t } = useTranslation("booth");
+
   return (
     <S.Container imgurl={data.boothThumbnailUrl} to={`/booth/${data.id}`}>
       <S.Info>
-        대기 | <S.Waitings>{data.boothWaitings ?? "?"}팀</S.Waitings>
+        {t("waiting")} | <S.Waitings>{data.boothWaitings ?? "?"}</S.Waitings>
         <br />
         <S.Faculty>
           <Br content={data.boothFaculty} />
